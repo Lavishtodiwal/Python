c2f6efeacbaa19d36507b6856e778f676356e475
Lavish Todiwal <lavishtodiwal7455@gmail.com>
2025-02-17 12:54:55 +0530

    changed SameLineOutput file

--- Day3/6.SameLineOutput.py
+++ Day3/6.SameLineOutput.py
@@ -1,4 +1,5 @@
-a = "12"
-b = int(a)
-print(id(a))
-print(id(b))
+print("Hello", end=" ")
+print("World")
+
+#end is use to print the output in the same line and if we give the end=" " then it will print the output in the same line with some space between the two values.
+# and sep=" " is used to seprate the multiple values in the same line with some sepration as we give in the sep="-"
