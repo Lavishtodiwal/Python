376440e25c88f75565f4c38893f87db72cae8b76
Lavish Todiwal <lavishtodiwal7455@gmail.com>
2025-02-17 15:06:57 +0530

    some programs

---
+++ Day3/7.AreaOfCircle.py
@@ -0,0 +1,4 @@
+radius = int(input("Enter the radius of the circle :"))
+area = (radius * radius * 3.14)
+
+print("Area of the circle as given radius:",area )
---
+++ Day3/8.AreaOfRectangle.py
@@ -0,0 +1,5 @@
+length = int(input("enter the length of the rectangle:"))
+width = int(input("enter the width of the rectangle:"))
+
+area = (length * width)
+print("Area of the Rectangle is : ", area)
